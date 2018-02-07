53c0771b7ccda33b957034a4e6f3d6f37721fe3c
jesusantguerrero <me@jesusantguerrero.com>
2018-02-06 22:59:19 -0400

    step-04 backend

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const os = request('os');
 const express = require('express');
 const app = express();
+const socketIO = require('socket.io');
 
 app.use( express.static(path.resolve(__dirname, 'work')))
 
@@ -17,6 +19,46 @@ app.get('/codelab-examples/:step/:page', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'codelab-examples', step, `${page ? page : 'index'}.html`));
 });
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log('running on port 5000');
+})
+
+const io = socketIO.listen(server);
+
+io.sockets.on('connection', (client) => {
+
+  client.on('message', (message) => {
+    console.log(`client said:${message}`);
+    client.emit('message', message);
+  })
+
+  client.on('create or join', (room) => {
+    console.log(`creating or join room: ${room}`);
+    const clientsCount = io.sockets.sockets.length;
+    
+    if (clientsCount <= 2) {
+      console.log(`${room} has ${clientsCount} client(s)`)
+      client.join(room)
+      if (clientsCount == 1) {
+        client.emit('created', room, client.id)
+      } else {
+        console.log(` client id ${client.id} has joined ${room}`)
+        client.emit('joined', room, client.id)
+        io.send.in(room).emit('ready')
+      }
+    } else {
+      client.emit('full', room);
+    }
+  })
+
+  client.on('ipAddr', () => {
+    const iFaces = os.networkInterfaces();
+    for (let dev in iFaces) {
+      iFaces[dev].forEach((details) => {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          client.emit('ipAddr', details.address)
+        }
+      })
+    }
+  })
 })
