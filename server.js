bf389e0239898d14cbffefeb77306b7d62b7f78f
jesusantguerrero <me@jesusantguerrero.com>
2018-02-14 19:37:25 -0400

    step-04 frontend

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const os = request('os');
+const os = require('os');
 const express = require('express');
 const app = express();
 const socketIO = require('socket.io');
@@ -9,7 +9,9 @@ app.use( express.static(path.resolve(__dirname, 'work')))
 
 app.get('/:page', (req, res) => {
   const page = req.params.page || 'index';
-  res.sendFile(path.resolve(__dirname, 'work', `${page}.html`))
+  if (!page.includes('favicon') && !page.includes('robots')) {
+    res.sendFile(path.resolve(__dirname, 'work', `${page}.html`));
+  }
 })
 
 app.use( express.static(path.resolve(__dirname)))
@@ -23,28 +25,34 @@ const server = app.listen(5000, () => {
   console.log('running on port 5000');
 })
 
+//  socket
 const io = socketIO.listen(server);
 
 io.sockets.on('connection', (client) => {
-
+  
+  // receiving messages 
   client.on('message', (message) => {
     console.log(`client said:${message}`);
     client.emit('message', message);
   })
 
   client.on('create or join', (room) => {
+    const clientsInRoom = io.sockets.adapter.rooms[room];
+    const clientsCount = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
     console.log(`creating or join room: ${room}`);
-    const clientsCount = io.sockets.sockets.length;
-    
-    if (clientsCount <= 2) {
+    console.log(clientsCount);
+
+    // join or create stuff
+    if (clientsCount < 2) {
       console.log(`${room} has ${clientsCount} client(s)`)
       client.join(room)
-      if (clientsCount == 1) {
+
+      if (clientsCount == 0) {
         client.emit('created', room, client.id)
       } else {
-        console.log(` client id ${client.id} has joined ${room}`)
         client.emit('joined', room, client.id)
-        io.send.in(room).emit('ready')
+        client.send().in(room).emit('ready')
+        console.log(` client id ${client.id} has joined ${room}`)
       }
     } else {
       client.emit('full', room);
@@ -61,4 +69,5 @@ io.sockets.on('connection', (client) => {
       })
     }
   })
+
 })
---
+++ work/js/chat.js
@@ -0,0 +1,47 @@
+
+class Chat {
+  constructor() {
+    this.isInitiator;
+    this.socket = io.connect();
+    this.room = null;
+  }
+  
+  boot() {
+    this.room = prompt("Enter room name:");
+    this.listen()
+    if (this.room !== "") {
+      console.log(`Message from client: Asking to join room ${this.room}`);
+      this.socket.emit('create or join', this.room.toLowerCase());
+    }
+  }
+
+  listen() {
+    this.socket.on('created', (room, clientId) => {
+      this.isInitiator = true;
+    });
+    
+    this.socket.on('full', (room) => {
+      console.log(`Message from client: Room ${room} is full :^(`);
+    });
+    
+    this.socket.on('ipAddr', (ipaddr) => {
+      console.log(`Message from client: Server IP address is ${ipaddr}`);
+    });
+    
+    this.socket.on('joined', (room, clientId) => {
+      this.isInitiator = false;
+    });
+  }
+}
+
+window.document.onreadystatechange = (e) => {
+  if ( document.readyState === 'complete') {
+    const chat = new Chat()
+    chat.boot();
+  } 
+}
+
+
+
+
+
